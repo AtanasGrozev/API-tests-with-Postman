9c0b1e23df6774ab043562157a574ed78250bca5
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-04-17 09:54:24 +0300

    .Upload IdeaCenter API testing with Postman and RestSharp

---
+++ IdeaCenter-Postman-Newman-RestSharp/IdeaAPITest-Atanas/IdeaAPITest/Models/ApiResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IdeaAPITest.Models
+{
+    public  class ApiResponseDTO // от целия content в respons-a мапваме само тази msg и id.
+    {
+        [JsonPropertyName("msg")]
+        public string Msg {  get; set; }
+
+        [JsonPropertyName("id")]
+        public string IdeaId{ get; set; }
+    }
+}
---
+++ IdeaCenter-Postman-Newman-RestSharp/IdeaAPITest-Atanas/IdeaAPITest/Models/IdeaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IdeaAPITest.Models
+{
+    public class IdeaDTO // играе ролата на Body от Postman, тази информация изпращаме.
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+    }
+}
